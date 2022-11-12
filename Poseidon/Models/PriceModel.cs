a76d577dc70b093373c2a91ff13153f42ef33740
kdgilang <gilangputra@icloud.com>
2022-11-12 22:07:50 +0800

    init price model

---
+++ Poseidon/Models/PriceModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Poseidon.Models
+{
+    public class PriceModel
+    {
+        public double Price { get; set; }
+
+        public string PriceText { get; set; }
+    }
+}
